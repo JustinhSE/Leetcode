80e7d48677f5eab0f5cc6da4317be0dfd6fa1c38
Justin H <84724234+JustinhSE@users.noreply.github.com>
2024-10-12 22:48:24 -0400

    Time: 4 ms (76.04%), Space: 44.6 MB (30.43%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,35 +10,52 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        //ptrs
-        List<ListNode> ptrs = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        //solution LL 
-        ListNode result = new ListNode();
-        ListNode ptr = result; //pointing to result
-
-        for(int i = 0 ; i < lists.length; i++){
-            ListNode p = new ListNode();
-            p = lists[i];
-            ptrs.add(p);
-        } 
-        while(true){
-            int min = Integer.MAX_VALUE;
-            int index = 0;
-            for(int j = 0; j < ptrs.size(); j++){
-                if(ptrs.get(j) == null){ continue; }
-                if(min > ptrs.get(j).val){
-                    min = ptrs.get(j).val; 
-                    index = j;
-                }
+        for(ListNode list: lists){
+            ListNode ptr = list;
+            while(ptr != null){
+                pq.add(ptr.val);
+                ptr = ptr.next; 
             }
-            if(min == Integer.MAX_VALUE){return result.next;}
-            ptr.next = new ListNode(min);
-            ptr = ptr.next; 
-            ListNode temp = ptrs.get(index).next; 
-            ptrs.remove(index); 
-            ptrs.add(index, temp);
         }
+        ListNode result = new ListNode();
+        ListNode ptr = result;
+        int size = pq.size();
+        for(int i = 0; i < size; i++){
+            ptr.next = new ListNode(pq.poll());
+            ptr = ptr.next;
+        }
+        return result.next;
+        // //ptrs
+        // List<ListNode> ptrs = new ArrayList<>();
+
+        // //solution LL 
+        // ListNode result = new ListNode();
+        // ListNode ptr = result; //pointing to result
+
+        // for(int i = 0 ; i < lists.length; i++){
+        //     ListNode p = new ListNode();
+        //     p = lists[i];
+        //     ptrs.add(p);
+        // } 
+        // while(true){
+        //     int min = Integer.MAX_VALUE;
+        //     int index = 0;
+        //     for(int j = 0; j < ptrs.size(); j++){
+        //         if(ptrs.get(j) == null){ continue; }
+        //         if(min > ptrs.get(j).val){
+        //             min = ptrs.get(j).val; 
+        //             index = j;
+        //         }
+        //     }
+        //     if(min == Integer.MAX_VALUE){return result.next;}
+        //     ptr.next = new ListNode(min);
+        //     ptr = ptr.next; 
+        //     ListNode temp = ptrs.get(index).next; 
+        //     ptrs.remove(index); 
+        //     ptrs.add(index, temp);
+        // }
 
     }
 }
