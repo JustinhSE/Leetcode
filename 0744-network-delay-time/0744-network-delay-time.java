f036807268a75515a8c2509422611931885b92ec
Justin H <84724234+JustinhSE@users.noreply.github.com>
2024-10-16 13:06:03 -0400

    Time: 28 ms (17.44%), Space: 48.3 MB (56.46%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,41 +1,28 @@
-
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        // Create adjacency list representation of the graph
-        Map<Integer, List<int[]>> graph = new HashMap<>();
-        for (int[] time : times) {
-            graph.computeIfAbsent(time[0], key -> new ArrayList<>()).add(new int[] { time[1], time[2] });
+        Map<Integer, List<int[]>> map = new HashMap<>();
+        for(int[] t: times){
+            map.computeIfAbsent(t[0], key -> new ArrayList<>()).add(new int[] {t[1], t[2]}); //node, weight
         }
-        //node, node weight
-        // (node, weight)
 
-        // Priority queue to store (weight, node) pairs
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[1] - b[1]); 
-        heap.add(new int[] { k,0 }); //node weight 
-
-        Set<Integer> seen = new HashSet<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        pq.add(new int[] {k,0});
+        Set<Integer> set = new HashSet<>();
         int time = 0;
-
-        while (!heap.isEmpty()) {
-            int[] current = heap.poll();
-            int node = current[0], weight = current[1];
-
-            if (seen.contains(node)) {
-                continue;
-            }
-
-            seen.add(node);
-            time = weight;
-
-            if (!graph.containsKey(node)) {
-                continue;
-            }
-
-            for (int[] neighbor : graph.get(node)) {
-                heap.add(new int[] {  neighbor[0], weight + neighbor[1] });
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int node = curr[0];
+            int weight = curr[1];
+            if(set.contains(node)){continue; }
+            set.add(node);
+            time = weight; 
+            if(!map.containsKey(node)){continue; }
+            for(int[] adjacents: map.get(node)){
+                pq.add(new int[] {adjacents[0], weight + adjacents[1] });
             }
         }
 
-        return seen.size() == n ? time : -1;
+        return set.size() == n ? time : -1;
+
     }
 }
