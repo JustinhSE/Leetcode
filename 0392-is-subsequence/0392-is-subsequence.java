bc496f8cc441059f8243c4446f2d84bc877f58a1
Justin H <84724234+JustinhSE@users.noreply.github.com>
2025-03-29 22:58:13 -0400

    Time: 0 ms (100%), Space: 41.7 MB (23.91%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         if(s.isEmpty()){return true;}
         int index = t.indexOf(s.charAt(0)); 
-        if(s.length() == 0 || (t.length() == index-1  && s.length() == 1)){ return true; }
+        if(s.length() == 0 || (index > 0  && s.length() == 1)){ return true; }
         if(s.length() > t.length() || index == -1 ){ return false; }
         return isSubsequence(s.substring(1) , t.substring(index + 1)); 
     }
