b6df7512b42a4d634be91e7fce593b893d95f938
Justin H <84724234+JustinhSE@users.noreply.github.com>
2025-07-13 00:05:52 -0400

    Time: 3 ms (90.95%), Space: 21.2 MB (80.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,7 +3,6 @@ class Solution:
         freq = defaultdict(int)
         for item in nums:
             freq[item] += 1
-        print(freq)
         top_k = sorted(freq.items(), key=lambda x: x[1])[-k:] 
         return [key for key, value in top_k]   
 
