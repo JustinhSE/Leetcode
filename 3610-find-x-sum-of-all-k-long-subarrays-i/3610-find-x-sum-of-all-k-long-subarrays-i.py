e80215a83868f202930f1c6306a252b5847c8346
Justin H <84724234+JustinhSE@users.noreply.github.com>
2025-07-13 20:00:32 -0400

    Time: 37 ms (17.27%), Space: 17.8 MB (82.24%) - LeetHub

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        #nums = [1,1,2,2,3,4,2,3], k = 6, x = 2
+        # i = [0,2],s = lst of it        
+        result = []
+        for i in range(0,len(nums) - k+1):
+            lst = nums[i:i+k]
+            s = set(lst)
+            if len(s) < x:
+                result.append(sum(lst))
+                continue
+            map = defaultdict(int)
+            for n in lst:
+                map[n] += 1
+            print(map)
+            top_x = sorted(map.items(), key = lambda x: (x[1], x[0]), reverse = True)[:x]
+            print(top_x)
+            keys = [k for k, v in top_x]
+            answer = filter(lambda num: num in keys, lst)
+            result.append(sum(answer))
+        return result
+
+
+
+
