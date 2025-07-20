87d372fc24fd62ba9729819ce8d7e6bf62e3f39c
Justin H <84724234+JustinhSE@users.noreply.github.com>
2025-07-20 18:44:47 -0400

    Time: 196 ms (22.56%), Space: 17.8 MB (91.92%) - LeetHub

--- 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.py
+++ 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.py
@@ -1,13 +1,12 @@
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
-        #sorting based length 
+        # sorting based length 
         # comparing each with the next highest double for loop nest 
-        strings = sorted(words, key = len)
         pairs = 0
-        for i in range(len(strings)):
-            curr = set(list(strings[i]))
-            for j in range(i+1, len(strings)):
-                pairs += 1 if set(list(strings[j])) == curr else 0
+        for i in range(len(words)):
+            curr = set(list(words[i]))
+            for j in range(i+1, len(words)):
+                pairs += 1 if set(list(words[j])) == curr else 0
         return pairs
 
         #["aba", "bac", "aabb","abcd","aabc"]
